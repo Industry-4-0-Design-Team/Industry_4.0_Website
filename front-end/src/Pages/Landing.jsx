2a543b98f391fedf2d1a82b1ca5002c1db97efc0
Gateek <chandakgateek@gmail.com>
2024-11-19 18:25:49 -0500

    fixed gear

--- front-end/src/Pages/Landing.jsx
+++ front-end/src/Pages/Landing.jsx
@@ -2,9 +2,9 @@ import '../Styles/Landing.css'
 
 const LandingPage = () => {
     return (
-        <div className="flex flex-col items-center w-full h-fit">
+        <div className="flex flex-col items-center w-full h-screen">
             <div className="flex flex-col gap-2">
-                <h1 className='w-full text-center text-8xl mt-28 text-primaryText font-black'>
+                <h1 className='w-full text-center text-8xl mt-20 text-primaryText font-black'>
                     INDUSTRY 4.
                     <img src="/Gears/yellow_gear_in_4.0.svg" className="spinning-image ml-1 w-20 h-20 inline-block relative -top-2" draggable="false"/>
                 </h1>
@@ -13,8 +13,8 @@ const LandingPage = () => {
                     <button className="text-third font-bold bg-secondary px-4 py-1 rounded-full mr-2 text-lg w-40 h-14">SIGN UP</button>
                     <button className="text-secondary font-bold border border-secondary px-4 py-1 rounded-full text-lg w-40 h-14">SPONSORS</button>
                 </div>
-                <div>
-                    <img src="/Vector.svg" alt="gear" className='absolute right-0' />
+                <div className="mt-14">
+                    <img src="/Vector.png" alt="gear" className='fixed right-0 bg-transparent' />
                 </div>
             </div>
         </div>
