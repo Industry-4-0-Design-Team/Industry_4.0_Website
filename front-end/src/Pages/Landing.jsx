4363222291c9b321a179c4542902496cb680dbe6
Abeer Das <abeerdas647@gmail.com>
2024-11-27 00:22:36 -0500

    mobile responsiveness

--- front-end/src/Pages/Landing.jsx
+++ front-end/src/Pages/Landing.jsx
@@ -1,11 +1,12 @@
-import "../Styles/Landing.css";
-import Button from "../Components/Button";
+import '../Styles/Landing.css';
+import Button from '../Components/Button';
 
 const LandingPage = () => {
     return (
         <div className="flex flex-col items-center w-full h-screen justify-center">
             {/* Main content container */}
-            <div className="flex flex-col items-center custom-lg:mt-[380px] mt-[550px]">
+            <div className="flex flex-col items-center mt-[250px]">
+            <div className="flex flex-col items-center  mt-[550px]">
                 {/* Left side (Industry 4. and gear) */}
                 <div className="sm:flex items-center justify-center w-full">
                     <h1 className="text-center text-8xl text-primaryText font-900 small:text-5xl small:font-600">
@@ -33,35 +34,21 @@ const LandingPage = () => {
                     </div>
                 </div>
             </div>
-        {/* Right side (Gear up to go beyond and buttons) */}
-        <div className="flex justify-center md:justify-end w-full mt-3 md:mt-6 lg:ml-[5rem] z-20">
-          <div className="text-center md:text-right lg:text-right lg:mr-10">
-            <h1 className="text-[#EEE6FF] font-900 text-lg md:text-3xl lg:text-4xl lg:text-right">
-              GEAR UP TO GO BEYOND
-            </h1>
-            <div className="mt-6 flex flex-col items-center md:flex-row gap-5 md:justify-end">
-              <Button text="SIGN UP" />
-              <button className="text-secondary font-900 border border-secondary px-4 py-1 rounded-full text-sm md:text-lg w-32 h-12 md:w-40 md:h-14 transition-colors duration-300 hover:bg-yellow-300/20">
-                SPONSORS
-              </button>
-            </div>
-          </div>
-        </div>
-      </div>
 
-      {/* Gear image at the bottom */}
-      <div className="mt-[22rem] h-72 relative z-1 clip-top-half">
-        <img
-          src="/Vector.svg"
-          alt="gear"
-          className="bg-transparent spinning-image2 align-center mt-[-24rem]"
-          style={{
-            maxWidth: "clamp(900px, 40vw + 3300px, 4900px)",
-          }}
-        />
-      </div>
-    </div>
-  );
+            {/* Gear image at the bottom */}
+            <div className="mt-[22rem] h-72 relative z-1 clip-top-half">
+                <img
+                    src="/Vector.svg"
+                    alt="gear"
+                    className="bg-transparent spinning-image2 align-center mt-[-24rem]"
+                    style={{
+                        maxWidth: 'clamp(900px, 40vw + 3300px, 4900px)',
+                    }}
+                />
+            </div>
+        </div>
+        </div>
+    );
 };
 
 export default LandingPage;
