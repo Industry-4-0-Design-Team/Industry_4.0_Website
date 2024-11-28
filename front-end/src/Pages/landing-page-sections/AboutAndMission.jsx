afcc64c96a21bd830aa471fd818dcc2da085933f
Gateek <chandakgateek@gmail.com>
2024-11-28 18:01:21 -0500

    fixed about section

--- front-end/src/Pages/landing-page-sections/AboutAndMission.jsx
+++ front-end/src/Pages/landing-page-sections/AboutAndMission.jsx
@@ -1,39 +1,36 @@
 const AboutAndMission = () => {
     return (
-      <div id="about" className="relative z-[10]">
-        <div className="flex mt-48 flex-col custom-lg:flex-row justify-between items-center custom-lg:items-start gap-8">
+      <div id="about" className="relative z-[10] w-full">
+        <div className="flex mt-52 flex-col justify-center items-center gap-10">
           {/* About Section */}
           <div
-            className="flex flex-col custom-lg:ml-[8rem] items-center custom-lg:items-start"
-            data-aos="fade-right"
-            data-aos-offset="50"
-            data-aos-delay="100"
-            data-aos-duration="700"
-            data-aos-once="true"
-          >
+            className="flex flex-col w-full items-center justify-center">
             <div
-              className="flex items-center ml-[2rem] mt-[5rem] custom-lg:mt-[3rem]"
-              data-aos="fade-right"
+              className="w-full flex justify-center items-center text-center mb-8"
+              data-aos="fade-down"
               data-aos-offset="50"
-              data-aos-delay="300"
+              data-aos-delay="100"
               data-aos-duration="700"
               data-aos-once="true"
             >
-              <h1 className="subtitle text-[24px] custom-lg:text-[54px] sm:text-4xl text-center custom-lg:text-left mt-10 z-10">
-                Ab
+              <h1 className="subtitle text-9xl z-10 tracking-widest">
+                AB
               </h1>
               <img
                 src="/Gears/yellow_gear_in_4.0.svg"
-                className="ml-1 mr-1 mt-[4.1rem] w-6 h-6 custom-lg:w-8 custom-lg:h-8 inline-block relative -top-2 clip-top-half"
+                className="ml-1 mr-3 mt-[4.1rem] w-24 h-24 inline-block relative -top-8 clip-top-half"
                 draggable="false"
               />
-              <h1 className="subtitle text-[24px] custom-lg:text-[54px] sm:text-4xl text-center custom-lg:text-left mt-10 z-10">
-                ut Us
+              <h1 className="subtitle text-9xl  z-10 tracking-widest">
+                UT US
               </h1>
             </div>
-            <h2
-              className="body px-8 text-center custom-lg:text-left custom-lg:text-[24px] md:text-[20px] mt-4 z-10 max-w-[800px]"
-            >
+            <p className="body px-20 text-left custom-lg:text-[24px] md:text-[20px] mt-4  mb-8 z-10 w-full"
+               data-aos="zoom-in"
+               data-aos-offset="50"
+               data-aos-delay="300"
+               data-aos-duration="700"
+               data-aos-once="true">
               We are an organization committed to empowering students in
               Management Sciences by fostering a hands-on learning environment.
               Through case studies, workshops, and leadership events, we provide
@@ -43,7 +40,7 @@ const AboutAndMission = () => {
               connections. Through mentorship, career-focused workshops, and a
               supportive community, we prepare them to excel in the tech-driven
               future of Industry 4.0.
-            </h2>
+            </p>
           </div>
   
           {/* Mission Section */}
@@ -90,19 +87,31 @@ const AboutAndMission = () => {
         <div className="w-full text-center mt-14 text-white">
           <ul className="flex w-full text-center items-center justify-around">
             <li>
-              <div>
+              <div data-aos="fade-right"
+                   data-aos-offset="50"
+                   data-aos-delay="500"
+                   data-aos-duration="700"
+                   data-aos-once="true">
                 <h1 className="font-black text-5xl">500+</h1>
                 <h3>COMPETITORS</h3>
               </div>
             </li>
             <li>
-              <div>
+              <div data-aos="fade-right"
+                   data-aos-offset="50"
+                   data-aos-delay="700"
+                   data-aos-duration="700"
+                   data-aos-once="true">
                 <h1 className="font-black text-5xl">25+</h1>
                 <h3>EVENTS</h3>
               </div>
             </li>
             <li>
-              <div>
+              <div data-aos="fade-right"
+                   data-aos-offset="50"
+                   data-aos-delay="900"
+                   data-aos-duration="700"
+                   data-aos-once="true">
                 <h1 className="font-black text-5xl">50+</h1>
                 <h3>ORGANIZERS</h3>
               </div>
@@ -111,7 +120,7 @@ const AboutAndMission = () => {
         </div>
   
         {/* Oscillating Gear */}
-        <div className="flex justify-center ml-[8rem] mt-24 mb-32">
+        <div className="flex justify-center ml-[8rem] mt-40 mb-32">
           <img
             src="/Gears/MissionGear.svg"
             className="oscillate w-[320px] h-auto"
