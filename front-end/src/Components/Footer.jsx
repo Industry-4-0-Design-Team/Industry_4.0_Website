cd109f6d8e98eefe138669de5c349b29740996ff
Abeer Das <abeerdas647@gmail.com>
2024-12-03 02:20:15 -0500

    added gear spin

--- front-end/src/Components/Footer.jsx
+++ front-end/src/Components/Footer.jsx
@@ -1,4 +1,14 @@
+import { useState } from "react";
+
 const Footer = () => {
+  
+  const [isSpinning, setIsSpinning] = useState(false);
+
+  const handleGearClick = () => {
+    setIsSpinning(true);
+    setTimeout(() => setIsSpinning(false), 500); // Remove spinning class after animation
+  };
+
   return (
     <div className="relative border-t border-gray-700">
       {/* Divider */}
@@ -8,7 +18,13 @@ const Footer = () => {
       <footer className="flex flex-col custom-lg:flex-row justify-between items-center custom-lg:items-end py-[38px] px-[92px]">
         {/* Left Side: Gear and Contact Email */}
         <div className="flex items-center space-x-4 mb-4 custom-lg:mb-0">
-          <img src="Gears/FooterGear.svg" alt="Footer Gear" className="w-6 h-6" />
+          <img
+            src="Gears/FooterGear.svg"
+            alt="Footer Gear"
+            className={`w-6 h-6 cursor-pointer transition-transform ${isSpinning ? "spin-gear" : ""
+              }`}
+            onClick={handleGearClick}
+          />
           <a href="mailto:industry4team@uwaterloo.ca" target="_blank">
             <span className="bg-gray-800 text-white py-1 px-3 rounded-full text-sm hover:bg-gray-700 transition-colors">
               industry4team@uwaterloo.ca
