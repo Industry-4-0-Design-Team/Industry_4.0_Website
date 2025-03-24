be1fa319e14e6d487684156b17dc836c3cd4689e
Abeer Das <67808617+AbeerDas@users.noreply.github.com>
2025-03-24 16:32:16 -0400

    responsiveness+hover effects+styling fixes+clickable compoenents

--- front-end/src/Components/EventComponent.jsx
+++ front-end/src/Components/EventComponent.jsx
@@ -1,25 +1,34 @@
-//NEED TO MAKE RESPONSIVE
-
-function EventComponent ({ title, eventDate, linkPath, imagePath, primaryTag, secondaryTag}){
-return(
-    <div className="bg-[#2B2A2D] rounded-2xl w-2/3  mx-auto px-5 py-5 flex flex-col">
-        <img src={imagePath} className="mx-auto w-full object-fill"/>
-        <div className="flex flex-row gap-2 items-center text-center justify-start mt-3">
-            <h1 className="text-white text-sm rounded-2xl w-max px-4 py-[5px] bg-[#6F52AF]">{primaryTag}</h1>
-            <h1 className="border border-white text-white text-sm rounded-2xl w-max px-4 py-[5px]">{secondaryTag}</h1>
-        </div>
-        <div>
-            <h1 className="text-primaryText text-4xl font-bold mt-5">{title}</h1>
-        </div>
-        <div className="flex flex-row justify-between items-end">
-            <h1 className="text-primaryText">{eventDate}</h1>
-            <a href={linkPath} target="_blank" className="rounded-full p-1 hover:bg-black">
-                <img className='w-7 h-7'
-                src="/externalLink.svg"/>
-            </a>
-        </div>
-    </div>
-)}
-
+function EventComponent({ title, eventDate, linkPath, imagePath, primaryTag, secondaryTag, animationDirection }) {
+    return (
+        <a 
+            href={linkPath} 
+            target="_blank" 
+            className="group bg-[#2B2A2D] mb-6 rounded-2xl w-full sm:w-[580px] h-[500px] mx-auto px-8 py-8 flex flex-col transition-all duration-1500 hover:bg-[#222222] hover:scale-102 relative" 
+            data-aos={animationDirection}
+            data-aos-offset="100"
+            data-aos-delay="100"
+            data-aos-duration="1200"
+            data-aos-once="true"
+        >
+            <img 
+                src={imagePath} 
+                className="mx-auto w-full object-cover h-[16rem] rounded-t-xl transition-all duration-[700ms] ease-in-out group-hover:h-72" 
+            />
+            <div className="flex flex-row gap-2 items-center text-center justify-start mt-6">
+                <h1 className="text-white text-sm rounded-2xl w-max px-4 py-[5px] bg-[#6F52AF]">{primaryTag}</h1>
+                <h1 className="border border-white text-white text-sm rounded-2xl w-max px-4 py-[5px]">{secondaryTag}</h1>
+            </div>
+            <div>
+                <h1 className="text-primaryText text-4xl font-bold mt-5">{title}</h1>
+            </div>
+            <div className="flex flex-row justify-between items-end">
+                <h1 className="text-primaryText">{eventDate}</h1>
+            </div>
+            <div className="absolute bottom-3 right-3 p-2 rounded-full text-white">
+                <img className="w-6 h-6" src="/externalLink.svg" alt="pop-out icon" />
+            </div>
+        </a>
+    );
+}
 
 export default EventComponent;
--- front-end/src/Pages/EventPage.jsx
+++ front-end/src/Pages/EventPage.jsx
@@ -1,33 +1,61 @@
 import EventComponent from "../Components/EventComponent";
 
-function Events(){
-    return(
+function Events() {
+    const eventsData = [
+        {
+            title: "I4 Challenge",
+            primaryTag: "Case Competition",
+            secondaryTag: "Big Data Analytics",
+            imagePath: "/Event-Page/competitionPhoto.png",
+            eventDate: "March 8, 2025",
+            linkPath: "https://www.instagram.com/p/DHDx3zquZSP/",
+        },
+        {
+            title: 'Co-Op Conversations',
+            primaryTag: 'Career Advice',
+            secondaryTag: 'Data Science',
+            eventDate: 'November 21, 2024',
+            linkPath: 'https://www.instagram.com/p/DC0HuAZt7tw/',
+            imagePath: '/Event-Page/coopConversations.png',
+        },
+        {
+            title: 'Co-Op Conversations',
+            primaryTag: 'Career Advice',
+            secondaryTag: 'Data Science',
+            eventDate: 'November 21, 2024',
+            linkPath: 'https://www.instagram.com/p/DC0HuAZt7tw/',
+            imagePath: '/Event-Page/coopConversations.png',
+        },
+        // Add more events here as needed
+    ];
+
+    return (
         <div className="pt-20 bg-[#1b1b1b] w-screen">
-            {/* NEED TO MAKE THE HR CENTERED AND APPEAR DIRECTLY UNDER THE EVENTS TEXT */}
-            <div className="w-full text-center relative">
-                <img src="/hr.png" className=" absolute" alt="" />
-                <h1 className="text-primaryText text-5xl font-bold mx-auto">EVENTS</h1>
-            </div>
-            <div className="grid grid-cols-2 gap-2">
-                <EventComponent 
-                    title="I4 Challenge"
-                    primaryTag="Case Competition"
-                    secondaryTag="Big Data Analytics"
-                    imagePath="/Event-Page/competitionPhoto.png"
-                    eventDate="March 8, 2025"
-                    linkPath="https://google.com"
+            <div className="w-full mt-12 mb-12 text-center relative">
+                <h1 className="text-primaryText text-5xl font-bold mx-auto relative z-10">EVENTS</h1>
+                <img
+                    src="/hr.png"
+                    className="absolute ml-[1rem] left-1/2 transform -translate-x-1/2 mt-[-1.5rem] w-[15rem] sm:w-[20rem] md:w-[30rem] lg:w-[40rem] xl:w-[100rem]"
+                    alt="Horizontal Line"
                 />
-                <EventComponent
-                    title='Co-Op Conversations'
-                    primaryTag='Career Advice'
-                    secondaryTag='Data Science'
-                    eventDate='November 21, 2024'
-                    linkPath='https://google.com'
-                    imagePath='/Event-Page/coopConversations.png'
-            />
+            </div>
+
+            <div className="grid grid-cols-1 xxl:grid-cols-3 custom-xl:grid-cols-2 gap-4 xs:mx-[1rem] sm:mx-[4rem] lmd:mx-[5rem] lg:mx-[12rem]">
+                {eventsData.map((event, index) => (
+                    <EventComponent
+                        key={index}
+                        title={event.title}
+                        primaryTag={event.primaryTag}
+                        secondaryTag={event.secondaryTag}
+                        imagePath={event.imagePath}
+                        eventDate={event.eventDate}
+                        linkPath={event.linkPath}
+                        animationDirection={index % 2 === 0 ? "fade-right" : "fade-left"}
+                    />
+                ))}
             </div>
         </div>
-    )
+    );
 }
 
 export default Events;
